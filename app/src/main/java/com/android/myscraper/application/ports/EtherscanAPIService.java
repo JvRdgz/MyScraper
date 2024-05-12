ea7a535b4d5f40f2dd4976f39209378e58e6171a
JvRdgz <javier.rodriguezjr97@gmail.com>
2024-05-12 03:46:49 +0200

    Firebase configuration added. Implementing EtherscanAPIService

---
+++ app/src/main/java/com/android/myscraper/application/ports/EtherscanAPIService.java
@@ -0,0 +1,12 @@
+package com.android.myscraper.application.ports;
+
+import retrofit2.http.GET;
+
+public interface EtherscanAPIService {
+
+    /**
+     * en el @GET("") se supone que va la siguiente parte de la URL de la API. En este caso lo que hay que investigar
+     * es la diferencia entre las querys que se especifican como parámetros en el método y meterlos directamente en este GET("")
+     */
+    // @GET("?module=")
+}
---
+++ app/src/main/java/com/android/myscraper/domain/dtos/WalletFetchResult.java
@@ -0,0 +1,11 @@
+package com.android.myscraper.domain.dtos;
+
+import java.util.ArrayList;
+
+public class WalletFetchResult {
+    private ArrayList<Wallet> result;
+
+    public ArrayList<Wallet> getRestuls() {
+        return result;
+    }
+}
